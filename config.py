25e15647ffbcee5aab86b74cd7d1332ff8079ea4
Victor Lombardi <victor.lombardi@ibm.com>
2018-06-28 16:01:08 +0200

    remove credentials

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 #Twitter API credentials
-consumer_key  = '8InW60jwG61XtwGNyHFU4IbiK'
-consumer_secret  = 'AgsKb07ZS9MeWpM8qhOcxTruTgmpUMPELVULZzTt0A2y7sfc99'
-access_token  = '936177469853454336-tKbX8leTNrQG7uKZR5qS7hXQ1O3mJUA'
-access_secret  = 'fVEE9Bd8AvjSSHkm4CBUPgLMzq0TkhZI2S8GTeYajjH1y'
+consumer_key  = ''#####''
+consumer_secret  = ''#####''
+access_token  = '#####'
+access_secret  = '#####'
